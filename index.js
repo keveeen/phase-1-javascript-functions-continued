2fb3fb33421cce2fd611adc28f006945535fe1eb
kevin breid <kevinbreid@yahoo.com>
2022-06-10 17:10:35 -0700

    commit tree 350ce438cb4ebaf66cfc1933614fc1732ae52080

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// code your solution here
+function saturdayFun(rest =  'roller-skate'){
+
+    return `This Saturday, I want to ${rest}!`
+}
+
+function mondayWork(toDo = 'go to the office'){
+
+    return `This Monday, I will ${toDo}.`
+}
+
+function wrapAdjective(cym = '*'){
+
+    return function inner(txt = 'special'){
+
+        return `You are ${cym}${txt}${cym}!`
+    }
+}
+wrapAdjective('||')('a dedicated programmer')
